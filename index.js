8451c3fa8c4087dd1151d9ad7092af41a5ad4973
Camila M <camilaom89@gmail.com>
2022-02-18 23:13:59 -0300

    Criando Banco

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-testando o git do javascript
+class Cliente {
+    nome;
+    cpf;
+}
+
+class ContaCorrente {
+    agencia;
+    saldo;
+
+    sacar(valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+        }
+    }
+}
+
+const cliente1 = new Cliente();
+cliente1.nome = "Camila";
+cliente1.cpf = 12345678912;
+
+const cliente2 = new Cliente();
+cliente2.nome = "Joca";
+cliente2.cpf = 13132154466;
+
+
+const contaCorrenteCamila = new ContaCorrente();
+contaCorrenteCamila.saldo = 0;
+contaCorrenteCamila.agencia = 1001;
+
+
+console.log(contaCorrenteCamila.saldo);
+contaCorrenteCamila.saldo = 150;
+console.log(contaCorrenteCamila.saldo);
+contaCorrenteCamila.sacar(200);
+console.log(contaCorrenteCamila.saldo);
+
+console.log(cliente1);
+console.log(cliente2);
